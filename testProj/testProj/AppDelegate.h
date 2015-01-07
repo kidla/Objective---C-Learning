40ca33f2638deedfc87303e24002c3278de54ba8
kidla <kidkidla@gmail.com>
2015-01-07 10:46:15 +0800

    test

---
+++ testProj/testProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProj
+//
+//  Created by Ｋ on 2015/1/7.
+//  Copyright (c) 2015年 Wayi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testProj/testProj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testProj
+//
+//  Created by Ｋ on 2015/1/7.
+//  Copyright (c) 2015年 Wayi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
